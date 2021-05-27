22de27f06648d14905efc1f868731c5a51f13838
ilyin-kirill <79863551+ilyin-kirill@users.noreply.github.com>
2021-05-28 00:19:09 +0300

    TKR0

---
+++ TKR0/TKR0/Data/MvcMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TKR0.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TKR0.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
---
+++ TKR0/TKR0/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TKR0.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
